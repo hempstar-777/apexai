db2daaf49e1071e2978a7bd4d73667cf8dcbc8c0
hempstar-777 <chidoweywey@gmail.com>
2025-08-19 17:30:40 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App data-filename="pages/SovereignMigrationCommand" data-linenumber="129" data-visual-selector-id="pages/SovereignMigrationCommand129" />
+  </React.StrictMode>
+);
